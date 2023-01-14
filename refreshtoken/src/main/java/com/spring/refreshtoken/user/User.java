70ba66f557552a003534f44a88f2502aeed57227
lky8967 <lky8967@gmail.com>
2023-01-15 01:16:45 +0900

    개발 환경 구성

---
+++ refreshtoken/src/main/java/com/spring/refreshtoken/user/User.java
@@ -0,0 +1,2 @@
+package com.spring.refreshtoken.User;public class User {
+}
---
+++ refreshtoken/src/test/java/com/spring/refreshtoken/RefreshtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.refreshtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshtokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
