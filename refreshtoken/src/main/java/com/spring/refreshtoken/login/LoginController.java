377ef416d2e2caadecd641156d663d5b5ae9af18
lky8967 <lky8967@gmail.com>
2023-01-16 20:28:23 +0900

    기존 로그인 + 토큰값 추가

--- refreshtoken/src/main/java/com/spring/refreshtoken/login/LoginController.java
+++ refreshtoken/src/main/java/com/spring/refreshtoken/login/LoginController.java
@@ -1,6 +1,5 @@
 package com.spring.refreshtoken.login;
 
-//import com.spring.refreshtoken.jwt.JwtService;
 import com.spring.refreshtoken.jwt.JwtTokenProvider;
 
 //import com.spring.refreshtoken.jwt.Token;
@@ -16,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController {
 
     private final LoginService loginService;
-//    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 //    private final JwtService jwtService;
 
     // 로그인
@@ -24,10 +23,10 @@ public class LoginController {
     public String login(final HttpServletResponse response , @RequestBody LoginRequestDto loginRequestDto) {
 //        log.info("user email = {}", user.get("userEmail"));
         String login = loginService.login(loginRequestDto);
-//        String tokenDto = jwtTokenProvider.createToken(loginRequestDto.getUsername());
+        String tokenDto = jwtTokenProvider.createToken(loginRequestDto.getUsername());
 //        log.info("getroleeeee = {}", member.getRoles());
 //        jwtService.login(tokenDto);
-        return "로그인";
-//        return tokenDto;
+//        return "로그인";
+        return tokenDto;
     }
 }
